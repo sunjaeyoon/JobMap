74fec7f0e7db9a1d094ddab78bfbc0f67e8b0f4b
Tim <39254058+sunjaeyoon@users.noreply.github.com>
2020-12-23 01:03:26 -0500

    Globe

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import {React, Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import ReactGlobe from "react-globe";
+//import * as THREE from "three";
+//import { MTLLoader, OBJLoader } from "three-obj-mtl-loader";
+//import OrbitControls from "three-orbitcontrols";
+class ThreeScene extends Component {
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
-
-export default App;
+  render() {
+    return( 
+      <ReactGlobe/>
+    )
+}}
+export default ThreeScene;
