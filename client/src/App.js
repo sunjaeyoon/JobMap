78434f592112f8a585269432c2ec81ebefc2b45f
Tim <39254058+sunjaeyoon@users.noreply.github.com>
2020-12-25 13:42:41 -0500

    test labels

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,79 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ReactGlobe from "react-globe";
-//import * as THREE from "three";
-//import { MTLLoader, OBJLoader } from "three-obj-mtl-loader";
-//import OrbitControls from "three-orbitcontrols";
-class ThreeScene extends Component {
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/scale.css';
 
-  render() {
-    return( 
-      <ReactGlobe/>
-    )
-}}
-export default ThreeScene;
+function App() {
+  const markers = [
+    {
+      id: 'marker1',
+      city: 'Singapore',
+      color: 'red',
+      coordinates: [1.3521, 103.8198],
+      value: 50,
+    },
+    {
+      id: 'marker2',
+      city: 'New York',
+      color: 'blue',
+      coordinates: [40.73061, -73.935242],
+      value: 25,
+    },
+    {
+      id: 'marker3',
+      city: 'San Francisco',
+      color: 'orange',
+      coordinates: [37.773972, -122.431297],
+      value: 35,
+    },
+    {
+      id: 'marker4',
+      city: 'Beijing',
+      color: 'gold',
+      coordinates: [39.9042, 116.4074],
+      value: 135,
+    },
+    {
+      id: 'marker5',
+      city: 'London',
+      color: 'green',
+      coordinates: [51.5074, 0.1278],
+      value: 80,
+    },
+    {
+      id: 'marker6',
+      city: 'Los Angeles',
+      color: 'gold',
+      coordinates: [29.7604, -95.3698],
+      value: 54,
+    },
+  ];
+ 
+  // simple and extensive options to configure globe
+  const options = {
+    ambientLightColor: 'blue',
+    cameraRotateSpeed: 0.5,
+    focusAnimationDuration: 2000,
+    focusEasingFunction: ['Linear', 'None'],
+    pointLightColor: 'gold',
+    pointLightIntensity: 1.5,
+    globeGlowColor: 'blue',
+    markerTooltipRenderer: marker => `${marker.city} (${marker.value})`,
+  };
+ 
+  const [globe, setGlobe] = useState(null);
+  console.log(globe); // captured globe instance with API methods
+  
+  return (
+    <div className="App">
+      <ReactGlobe
+        markers={markers}
+        options={options}
+        height="99vh"
+        width="100vw"
+      />
+    </div>
+  );
+}
+
+export default App;
